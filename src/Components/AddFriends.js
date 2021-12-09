3506171e89791bc158c5be02e142e68fc81879e1
Sudeep Mishra <sudeep014@gmail.com>
2021-12-09 23:14:06 +0545

    added comments

--- src/Components/AddFriends.js
+++ src/Components/AddFriends.js
@@ -1,14 +1,27 @@
+//importing useState hook used to create and update state variable
 import { useState } from "react";
+
+//get data from localstorage if there is data else set empty array
 const savedFriends = localStorage.getItem("friend-list") ? JSON.parse(localStorage.getItem("friend-list")) : []
 function AddFriends() {
+    //state variables friendList, inputName, showInput
     const [friendList, setFriendList] = useState(savedFriends)
     const [inputName, setInputName] = useState("")
     const [showInput, setShowInput] = useState(false)
+
+    //function to add friend
     const addFriend = () => {
+        //create copy of friendList state and add new name to the copy
         let updatedFriendList = [...friendList, { name: inputName }]
+
+        //set the copy as original friendlist state
         setFriendList(updatedFriendList)
+
+        //save the copy in string format to localstorage
         localStorage.setItem("friend-list", JSON.stringify(updatedFriendList))
     }
+
+    // change showInput variable to negative of what it was (ie true if false and false if true)
     const toggleShow = ()=>{
         setShowInput(!showInput)
     }
@@ -24,6 +37,7 @@ function AddFriends() {
                         return (<li>{friend.name}</li>)
                     })}
                 </ul>
+                {/* show input if and only if showInput is true. if show inout is false do not show the input add add button */}
                 {showInput &&
                     <div className="searchbox">
                         <input type="text" id="fname" name="fname" value={inputName} onChange={(e) => { setInputName(e.target.value) }}></input>
