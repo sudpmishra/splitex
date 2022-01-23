e136b388166ed1ed8ec573ca5f5aa4e8ee0ad877
SUJINA MAHARJAN <suzeena.mhrjn@gmail.com>
2022-01-23 21:41:30 +0545

    Dashboard route added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './Dashboard.css';
+import logo from './../../logo.png';
 
 const Dashboard = () => {
   return (
     <>
-    Dashboard
+      <header>
+        <div className="header-img">
+          <img src={logo}></img>
+        </div>
+        <div className="header-right">
+          <img src=''></img>
+          <div className="header-righ-info">
+            <span>Sujina Maharjan</span>
+            <span>suzeena.mhrjn@gmail.com</span>
+          </div>
+          <div className="header-right-icon">
+            <span>p</span>
+            <span>p</span>
+          </div>
+        </div>
+      </header>
+      <hr/>
+      <footer>
+        <span>Copyright 2022, SplitEx</span>
+      </footer>
     </>
   );
 };
